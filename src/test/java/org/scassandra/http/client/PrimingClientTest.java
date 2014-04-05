fef0851445c10514b78a347cd4d8ce40e594a559
Christopher Batey <christopher.batey@gmail.com>
2014-04-05 12:20:59 -0600

    boyscout: removing duplicate test code

--- src/test/java/org/scassandra/http/client/PrimingClientTest.java
+++ src/test/java/org/scassandra/http/client/PrimingClientTest.java
@@ -3,6 +3,7 @@ package org.scassandra.http.client;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -15,14 +16,21 @@ public class PrimingClientTest {
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(PORT);
 
+    private PrimingClient underTest;
+
+    @Before
+    public void setup() {
+        underTest = new PrimingClient("localhost", PORT);
+    }
+
+
     @Test
     public void testPrimingEmptyResults() {
         //given
         stubFor(post(urlEqualTo("/prime")).willReturn(aResponse().withStatus(200)));
-        PrimingClient pc = new PrimingClient("localhost", PORT);
         PrimingRequest pr = new PrimingRequest("select * from people", Collections.<Map<String, String>>emptyList());
         //when
-        pc.prime(pr);
+        underTest.prime(pr);
         //then
         verify(postRequestedFor(urlEqualTo("/prime"))
                 .withHeader("Content-Type", equalTo("application/json; charset=UTF-8"))
@@ -33,14 +41,13 @@ public class PrimingClientTest {
     public void testPrimingWithMultipleRows() {
         //given
         stubFor(post(urlEqualTo("/prime")).willReturn(aResponse().withStatus(200)));
-        PrimingClient pc = new PrimingClient("localhost", PORT);
         List<Map<String,String>> rows = new ArrayList<>();
         Map<String, String> row = new HashMap<>();
         row.put("name","Chris");
         rows.add(row);
         PrimingRequest pr = new PrimingRequest("select * from people", rows);
         //when
-        pc.prime(pr);
+        underTest.prime(pr);
         //then
         verify(postRequestedFor(urlEqualTo("/prime"))
                 .withHeader("Content-Type", equalTo("application/json; charset=UTF-8"))
@@ -51,10 +58,9 @@ public class PrimingClientTest {
     public void testPrimingReadRequestTimeout() {
         //given
         stubFor(post(urlEqualTo("/prime")).willReturn(aResponse().withStatus(200)));
-        PrimingClient pc = new PrimingClient("localhost", PORT);
         PrimingRequest pr = new PrimingRequest("select * from people", PrimingRequest.Result.read_request_timeout);
         //when
-        pc.prime(pr);
+        underTest.prime(pr);
         //then
         verify(postRequestedFor(urlEqualTo("/prime"))
                 .withHeader("Content-Type", equalTo("application/json; charset=UTF-8"))
@@ -79,10 +85,9 @@ public class PrimingClientTest {
     public void testPrimingWriteRequestTimeout() {
         //given
         stubFor(post(urlEqualTo("/prime")).willReturn(aResponse().withStatus(200)));
-        PrimingClient pc = new PrimingClient("localhost", PORT);
         PrimingRequest pr = new PrimingRequest("select * from people", PrimingRequest.Result.write_request_timeout);
         //when
-        pc.prime(pr);
+        underTest.prime(pr);
         //then
         verify(postRequestedFor(urlEqualTo("/prime"))
                 .withHeader("Content-Type", equalTo("application/json; charset=UTF-8"))
@@ -93,11 +98,11 @@ public class PrimingClientTest {
     public void testPrimeFailed() {
         //given
         stubFor(post(urlEqualTo("/prime")).willReturn(aResponse().withStatus(500)));
-        PrimingClient pc = new PrimingClient("localhost", PORT);
         PrimingRequest pr = new PrimingRequest("select * from people", Collections.<Map<String, String>>emptyList());
         //when
-        pc.prime(pr);
+        underTest.prime(pr);
         //then
 
     }
+
 }
