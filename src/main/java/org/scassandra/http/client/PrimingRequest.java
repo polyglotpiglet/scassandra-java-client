e9d5d41ffd63e8758830a8a648d247a958073208
Christopher Batey <christopher.batey@gmail.com>
2014-05-14 19:27:55 +0100

    [#9] Adding toString to When object

--- src/main/java/org/scassandra/http/client/PrimingRequest.java
+++ src/main/java/org/scassandra/http/client/PrimingRequest.java
@@ -167,6 +167,14 @@ public class PrimingRequest {
         }
 
         @Override
+        public String toString() {
+            return "When{" +
+                    "query='" + query + '\'' +
+                    ", consistency=" + consistency +
+                    '}';
+        }
+
+        @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
