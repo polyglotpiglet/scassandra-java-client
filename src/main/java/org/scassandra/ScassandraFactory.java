3957b16f423649bb6f864cb921bf6385dbe44d00
Christopher Batey <christopher.batey@gmail.com>
2014-05-20 19:48:08 +0100

    adding factory method with default host and port

--- src/main/java/org/scassandra/ScassandraFactory.java
+++ src/main/java/org/scassandra/ScassandraFactory.java
@@ -4,4 +4,8 @@ public class ScassandraFactory {
     public static final Scassandra createServer(int binaryPort, int adminPort) {
         return new ScassandraRunner(binaryPort, adminPort);
     }
+
+    public static final Scassandra createServer() {
+        return new ScassandraRunner(8042, 8043);
+    }
 }
