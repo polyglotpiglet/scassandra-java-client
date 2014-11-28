8fc29853cad4f07dabd26fb973fb1b50460c86e1
Christopher Batey <christopher.batey@gmail.com>
2014-11-28 15:11:18 +0000

    0.4.1 release

--- src/test/java/org/scassandra/VersionIntegrationTest.java
+++ src/test/java/org/scassandra/VersionIntegrationTest.java
@@ -11,7 +11,7 @@ public class VersionIntegrationTest {
     public void getVersion() {
         scassandra.start();
         String version = scassandra.serverVersion();
-        assertEquals("0.4.1-SNAPSHOT", version);
+        assertEquals("0.4.1", version);
     }
 
 }
